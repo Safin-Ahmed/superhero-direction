bd49768bae0285314ba05f30d0421bf143c0c689
Safin-Ahmed <safin.ahmed2000@gmail.com>
2021-09-26 22:02:06 +0600

    Changed some texts

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,8 +5,8 @@ const Header = () => {
     return (
         <div>
             <div className="header-container">
-                <h1>Hire the top developers of the globe for your next project!</h1>
-                <p>The world is moving with technologies and army of websites and e-commerce.But we have the tech ninjas all prepared just for you</p>
+                <h1>Top 12 Developers to Build The Next Facebook and Google</h1>
+                <p>Uncle ZuckerBurg and Grandfather Sundar Pichai is constantly stealing informations of millions of user. That's why our superhero Elon Musk came to me and hired me to build a website listing the top developer warriors that he can hire to make his own facebook and google.</p>
                 <h1>Total Budget : 500 Million </h1>
             </div>
         </div>
