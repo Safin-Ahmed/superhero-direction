376bac5f527147e983c4c8e6ee237acdfa7c51c5
Safin-Ahmed <safin.ahmed2000@gmail.com>
2021-09-26 18:01:40 +0600

    developer card container components are fully completed

--- src/components/Developer/Developer.js
+++ src/components/Developer/Developer.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import './Developer.css';
+import { faFacebook, faYoutube } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
 const Developer = (props) => {
     const {img, key, name, skill, level, country, salary, facebook, youtube} = props.dev;
+    const facebookIcon = <FontAwesomeIcon size="2x" icon={faFacebook} />;
+    const youtubeIcon =  <FontAwesomeIcon size="2x" icon={faYoutube} />;
+    const cartIcon =  <FontAwesomeIcon size="lg" icon={faShoppingCart} />;
     return (
         <div className = "developer">
             <img src={img} alt="" />
-            <h3>Name: {name}</h3>
-            <h3>Skill: {skill}</h3>
-            <h3>Level: {level}</h3>
-            <h3>Country: {country}</h3>
-            <h3>Salary: {salary}</h3>
-            <button><a href= {facebook ? `${facebook}` : `#`}>Facebook</a></button>
-            <button><a href= {youtube ? `${youtube}` : `#`}>Youtube</a></button>
+            <h3>Name: <span>{name}</span></h3>
+            <h3>Skill: <span>{skill}</span></h3>
+            <h3>Level: <span>{level}</span></h3>
+            <h3>Country: <span>{country}</span></h3>
+            <h3>Salary: <span>$<span className="salary">{salary}</span></span></h3>
+            <button className="add-to-cart">{cartIcon} Add To Cart</button>
+            <button><a href= {facebook ? `${facebook}` : `#`}>{facebookIcon}</a></button>
+            <button><a href= {youtube ? `${youtube}` : `#`}>{youtubeIcon}</a></button>
         </div>
     );
 };
